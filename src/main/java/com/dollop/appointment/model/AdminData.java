58f00d0c1d969cb81ce20c89f44902f90bece3da
Nilesh Malviya <nilesh.malviya.er@gmail.com>
2022-06-22 19:40:25 +0530

    AdminData and AdminRegistationData are added

---
+++ src/main/java/com/dollop/appointment/model/AdminData.java
@@ -0,0 +1,79 @@
+
+package com.dollop.appointment.model;
+
+public class AdminData {
+	
+	private String firstName;
+	private String lastName;
+	private String dateOfBirth;
+	private String email;
+	private String mobileNumber;
+	private String address;
+	private String city;
+	private String state;
+	private String zipCode;
+	private String country;
+//	comment
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+}
