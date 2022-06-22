351b0fa171b9c93c9705861a5d8182831ee305c6
unknown <dipanshut06@gmail.com>
2022-06-22 18:02:55 +0530

    add adminRegistrationData

---
+++ src/main/java/com/dollop/appointment/model/AdminRegistrationData.java
@@ -0,0 +1,53 @@
+package com.dollop.appointment.model;
+
+public class AdminRegistrationData {
+	
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String mobileNumber;
+	private String password;
+	private String conPassword;
+	
+//	admin registration data
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getConPassword() {
+		return conPassword;
+	}
+	public void setConPassword(String conPassword) {
+		this.conPassword = conPassword;
+	}
+	
+	
+
+}
+
