b6d05c0679c61ffb5a52af8b04551506ff03e207
Piyush Jaiswal <jaiswal.piyush309@gmail.com>
2022-06-22 10:18:25 +0530

    Create Invoice View Class

---
+++ src/main/java/com/dollop/appointment/model/InvoiceView.java
@@ -0,0 +1,170 @@
+package com.dollop.appointment.model;
+
+public class InvoiceView 
+{
+	private String docFirstName;
+	private String docLastName;
+	private String docAddress;
+	private String docCity;
+	private String docState;
+	private String docCountry;
+	private String docPostalCode;
+	
+	private String patFirstName;
+	private String patLastName;
+	private String patAddress;
+	private String patCity;
+	private String patState;
+	private String patCountry;
+	private String patZipCode;
+	
+	private int paymentId;
+	private int patientId;
+	
+	private String cardNumber;
+	private String cardName;
+	private String dateTime;
+	private String paymentType;
+	private double amount;
+	private String invoiceDate;
+	private String orderId;
+	
+	public String getDocFirstName() {
+		return docFirstName;
+	}
+	public void setDocFirstName(String docFirstName) {
+		this.docFirstName = docFirstName;
+	}
+	public String getDocLastName() {
+		return docLastName;
+	}
+	public void setDocLastName(String docLastName) {
+		this.docLastName = docLastName;
+	}
+	public String getDocAddress() {
+		return docAddress;
+	}
+	public void setDocAddress(String docAddress) {
+		this.docAddress = docAddress;
+	}
+	public String getDocCity() {
+		return docCity;
+	}
+	public void setDocCity(String docCity) {
+		this.docCity = docCity;
+	}
+	public String getDocState() {
+		return docState;
+	}
+	public void setDocState(String docState) {
+		this.docState = docState;
+	}
+	public String getDocCountry() {
+		return docCountry;
+	}
+	public void setDocCountry(String docCountry) {
+		this.docCountry = docCountry;
+	}
+	public String getDocPostalCode() {
+		return docPostalCode;
+	}
+	public void setDocPostalCode(String docPostalCode) {
+		this.docPostalCode = docPostalCode;
+	}
+	public String getPatFirstName() {
+		return patFirstName;
+	}
+	public void setPatFirstName(String patFirstName) {
+		this.patFirstName = patFirstName;
+	}
+	public String getPatLastName() {
+		return patLastName;
+	}
+	public void setPatLastName(String patLastName) {
+		this.patLastName = patLastName;
+	}
+	public String getPatAddress() {
+		return patAddress;
+	}
+	public void setPatAddress(String patAddress) {
+		this.patAddress = patAddress;
+	}
+	public String getPatCity() {
+		return patCity;
+	}
+	public void setPatCity(String patCity) {
+		this.patCity = patCity;
+	}
+	public String getPatState() {
+		return patState;
+	}
+	public void setPatState(String patState) {
+		this.patState = patState;
+	}
+	public String getPatCountry() {
+		return patCountry;
+	}
+	public void setPatCountry(String patCountry) {
+		this.patCountry = patCountry;
+	}
+	public String getPatZipCode() {
+		return patZipCode;
+	}
+	public void setPatZipCode(String patZipCode) {
+		this.patZipCode = patZipCode;
+	}
+	public int getPaymentId() {
+		return paymentId;
+	}
+	public void setPaymentId(int paymentId) {
+		this.paymentId = paymentId;
+	}
+	public int getPatientId() {
+		return patientId;
+	}
+	public void setPatientId(int patientId) {
+		this.patientId = patientId;
+	}
+	public String getCardNumber() {
+		return cardNumber;
+	}
+	public void setCardNumber(String cardNumber) {
+		this.cardNumber = cardNumber;
+	}
+	public String getCardName() {
+		return cardName;
+	}
+	public void setCardName(String cardName) {
+		this.cardName = cardName;
+	}
+	public String getDateTime() {
+		return dateTime;
+	}
+	public void setDateTime(String dateTime) {
+		this.dateTime = dateTime;
+	}
+	public String getPaymentType() {
+		return paymentType;
+	}
+	public void setPaymentType(String paymentType) {
+		this.paymentType = paymentType;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public String getInvoiceDate() {
+		return invoiceDate;
+	}
+	public void setInvoiceDate(String invoiceDate) {
+		this.invoiceDate = invoiceDate;
+	}
+	public String getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(String orderId) {
+		this.orderId = orderId;
+	}	
+}
